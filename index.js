5083963a518ff480aa2fbc2649486d6bba2d5abb
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-13 08:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function init() {
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init(
+    Handlebars.registerPartial('recipeDetailsPartial',
+  document.getElementById("recipe-details-partial").innerHTML)
+  )
 })
